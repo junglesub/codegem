fca08fbe1e5b602713d9a05a5fe6006265f4bdf5
Sangdeveloper7 <leesanghyun9757@gmail.com>
2024-09-02 01:20:16 +0900

    feat: add google oauth detail and impl role

--- src/main/java/com/thc/realspr/domain/Tbuser.java
+++ src/main/java/com/thc/realspr/domain/Tbuser.java
@@ -3,6 +3,7 @@ package com.thc.realspr.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
 
 import java.util.UUID;
 
@@ -19,8 +20,14 @@ public class Tbuser {
     @Setter @Column(nullable = false) private String phone;
     @Setter @Column(nullable = false) private String mpic; // 프로필 사진
     @Setter @Column(nullable = false, length=2000000) @Lob private String content; // 본문
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Role role;
+
+
     protected Tbuser(){}
-    private Tbuser(String username, String password, String name, String nick, String phone, String mpic, String content) {
+    private Tbuser(String username, String password, String name, String nick, String phone, String mpic, String content, Role role) {
         this.username = username;
         this.password = password;
         this.name = name;
@@ -28,9 +35,11 @@ public class Tbuser {
         this.phone = phone;
         this.mpic = mpic;
         this.content = content;
+        this.role = role;
     }
+    // TODO: role 추가 해야 함 .
     public static Tbuser of(String username, String password){
-        return new Tbuser(username, password, "", "", "", "", "");
+        return new Tbuser(username, password, "", "", "", "", "", null);
     }
 
     @PrePersist
@@ -38,4 +47,9 @@ public class Tbuser {
         this.id = UUID.randomUUID().toString().replace("-", "");
         this.deleted = "N";
     }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+
 }
