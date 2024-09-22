78875b21f9c4ca63e56c85609aadb2ab008f7455
Sangdeveloper7 <leesanghyun9757@gmail.com>
2024-09-23 01:35:05 +0900

    create-user-not-compelet

--- src/main/java/com/thc/realspr/service/TbuserService.java
+++ src/main/java/com/thc/realspr/service/TbuserService.java
@@ -1,21 +1,21 @@
 package com.thc.realspr.service;
 
+import com.thc.realspr.domain.Tbuser;
 import com.thc.realspr.dto.DefaultDto;
 import com.thc.realspr.dto.GoogleLoginRequest;
 import com.thc.realspr.dto.GoogleLoginResponse;
 import com.thc.realspr.dto.TbuserDto;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 @Service
 public interface TbuserService {
-    public Map<String, Object> create(Map<String, Object> param);
-    public Map<String, Object> update(Map<String, Object> param);
-    public Map<String, Object> get(String id);
+    Tbuser create(TbuserDto.CreateReqDto param);
+    TbuserDto.DetailResDto detail(DefaultDto.DetailReqDto param);
 
     TbuserDto.CreateResDto access(String param) throws Exception;
 
     //    TbuserDto.CreateResDto access(String param) throws Exception;
     GoogleLoginResponse loginWithGoogle(GoogleLoginRequest request);
+
+    Tbuser loginWithGoogle(String credential);
 }
