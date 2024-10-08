18ed755f7cf11c7ca373221156ba755719a2cf62
junglesub <junglesubmarine@gmail.com>
2024-10-09 00:01:59 +0900

    feat(client): allfeed is same as mainfeed

--- src/main/front/src/pages/AllFeed.jsx
+++ src/main/front/src/pages/AllFeed.jsx
@@ -1,11 +1,68 @@
 import { Box, Button, CssBaseline, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import MyAppbar from "../components/MyAppbar";
 import MainDrawer from "../components/MainDrawer";
 import MainDisplay from "../components/MainDisplay";
+import FeedCard from "../components/FeedCard";
+import { useFetchBe } from "../tools/api";
+import InfiniteScroll from "react-infinite-scroller";
+import { removeDuplicates } from "../tools/tools";
 
 function AllFeed() {
-  return <MainDisplay>AllFeed</MainDisplay>;
+  const [allFeeds, setAllFeed] = useState([]);
+  const [allSeenFeedId, setAllSeenFeedId] = useState(new Set());
+  const [hasMore, setHasMore] = useState(true);
+  const fetch = useFetchBe();
+
+  const loadData = async () => {
+    const lastTimestamp = allFeeds.at(-1)?.sentAtEpoch || -1;
+    const data = await fetch(
+      `/kafeed/scrolllist?afterSentAt=${lastTimestamp}&all=y`
+    );
+    if (!Array.isArray(data)) return;
+    setAllFeed((prev) =>
+      removeDuplicates(
+        [
+          ...prev,
+          ...data.map((dd) => ({
+            author: "실명카톡방2",
+            sentAtEpoch: dd.sentAt,
+            createdAt: new Date(dd.sentAt * 1000),
+            id: dd.id,
+            content: dd.message,
+            files: dd.files,
+            img: dd.files[0], // Temp just for testing
+            subjectId: dd.subjectId,
+          })),
+        ],
+        "id"
+      )
+    );
+    if (data.length === 0) setHasMore(false);
+  };
+
+  const allFeedsToDisplay = allFeeds.filter(
+    (item) => !allSeenFeedId.has(item.subjectId)
+  );
+
+  return (
+    <MainDisplay>
+      <InfiniteScroll loadMore={loadData} hasMore={hasMore}>
+        {allFeedsToDisplay.map((item) => (
+          // <FeedItemNew
+          //   key={item.id}
+          //   item={item}
+          //   setAllSeenFeedId={setAllSeenFeedId}
+          // />
+          <FeedCard
+            key={item.id}
+            item={item}
+            setAllSeenFeedId={setAllSeenFeedId}
+          />
+        ))}
+      </InfiniteScroll>
+    </MainDisplay>
+  );
 }
 
 export default AllFeed;
