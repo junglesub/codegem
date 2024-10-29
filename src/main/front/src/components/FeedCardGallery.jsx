3925f411be418642958279f0f41af4deffeb267c
junglesub <junglesubmarine@gmail.com>
2024-10-29 15:03:07 +0900

    feat(client): more info link

--- src/main/front/src/components/FeedCardGallery.jsx
+++ src/main/front/src/components/FeedCardGallery.jsx
@@ -1,5 +1,5 @@
 import "./FeedCardGallery.css";
-import { Gallery, Item } from "react-photoswipe-gallery";
+import { Gallery, Item, useGallery } from "react-photoswipe-gallery";
 import { getExtensionFromUrl, isImage, isVideo } from "../tools/tools";
 
 import "photoswipe/dist/photoswipe.css";
@@ -11,35 +11,54 @@ const FeedCardGallery = ({ images = [] }) => {
   return (
     <div className="FeedCardGallery">
       <Gallery withDownloadButton>
-        <div className={`post-images images-${filteredImages.length}`}>
-          {filteredImages.slice(0, 3).map((url, index) =>
-            isImage(url) ? (
-              <FeedCardImageItem url={url} index={index} />
-            ) : // <img key={index} src={url} alt={`${index + 1}`} loading="lazy" />
-            isVideo(url) ? (
-              <video key={index} controls>
-                <source src={url} type={`video/${getExtensionFromUrl(url)}`} />
-                Your browser does not support the video tag.
-              </video>
-            ) : (
-              <>Unknown Data</>
-            )
-          )}
-          {filteredImages.length >= 4 && (
-            <div className="more-images-overlay">
-              {filteredImages.length > 5 && <span>+{images.length - 4}</span>}
-              <FeedCardImageItem url={images[3]} index={3} />
-              {filteredImages.slice(4).map((url, index) => (
-                <FeedCardImageItem url={url} index={index + 4} hidden={true} />
-              ))}
-
-              {/* <img key={1} src={images[3]} alt={`4`} loading="lazy" /> */}
-            </div>
-          )}
-        </div>
+        <FeedCardGalleryContent
+          images={images}
+          filteredImages={filteredImages}
+        />
       </Gallery>
     </div>
   );
 };
 
 export default FeedCardGallery;
+
+const FeedCardGalleryContent = ({ filteredImages, images }) => {
+  const { open } = useGallery();
+
+  return (
+    <div className={`post-images images-${filteredImages.length}`}>
+      {filteredImages.slice(0, 3).map((url, index) =>
+        isImage(url) ? (
+          <FeedCardImageItem url={url} index={index} />
+        ) : // <img key={index} src={url} alt={`${index + 1}`} loading="lazy" />
+        isVideo(url) ? (
+          <video key={index} controls>
+            <source src={url} type={`video/${getExtensionFromUrl(url)}`} />
+            Your browser does not support the video tag.
+          </video>
+        ) : (
+          <>Unknown Data</>
+        )
+      )}
+      {filteredImages.length >= 4 && (
+        <div className="more-images-overlay">
+          {filteredImages.length > 5 && (
+            <span
+              onClick={() => {
+                open(3);
+              }}
+            >
+              +{images.length - 4}
+            </span>
+          )}
+          <FeedCardImageItem url={images[3]} index={3} />
+          {filteredImages.slice(4).map((url, index) => (
+            <FeedCardImageItem url={url} index={index + 4} hidden={true} />
+          ))}
+
+          {/* <img key={1} src={images[3]} alt={`4`} loading="lazy" /> */}
+        </div>
+      )}
+    </div>
+  );
+};
