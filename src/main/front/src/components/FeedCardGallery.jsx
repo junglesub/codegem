5b380dcd14fe49bf7aedc7c2713f2dd6ef8b3811
junglesub <junglesubmarine@gmail.com>
2024-10-29 14:50:09 +0900

    feat(client): impl gallary for images

--- src/main/front/src/components/FeedCardGallery.jsx
+++ src/main/front/src/components/FeedCardGallery.jsx
@@ -1,31 +1,43 @@
 import "./FeedCardGallery.css";
+import { Gallery, Item } from "react-photoswipe-gallery";
 import { getExtensionFromUrl, isImage, isVideo } from "../tools/tools";
+
+import "photoswipe/dist/photoswipe.css";
+import FeedCardImageItem from "./FeedCardImageItem";
 
 const FeedCardGallery = ({ images = [] }) => {
   const filteredImages = images.filter((url) => isImage(url) || isVideo(url));
   if (filteredImages.length === 0) return <></>;
   return (
     <div className="FeedCardGallery">
-      <div className={`post-images images-${filteredImages.length}`}>
-        {filteredImages.slice(0, 3).map((url, index) =>
-          isImage(url) ? (
-            <img key={index} src={url} alt={`${index + 1}`} loading="lazy" />
-          ) : isVideo(url) ? (
-            <video key={index} controls>
-              <source src={url} type={`video/${getExtensionFromUrl(url)}`} />
-              Your browser does not support the video tag.
-            </video>
-          ) : (
-            <>Unknown Data</>
-          )
-        )}
-        {filteredImages.length >= 4 && (
-          <div className="more-images-overlay">
-            {filteredImages.length > 5 && <span>+{images.length - 4}</span>}
-            <img key={1} src={images[3]} alt={`4`} loading="lazy" />
-          </div>
-        )}
-      </div>
+      <Gallery withDownloadButton>
+        <div className={`post-images images-${filteredImages.length}`}>
+          {filteredImages.slice(0, 3).map((url, index) =>
+            isImage(url) ? (
+              <FeedCardImageItem url={url} index={index} />
+            ) : // <img key={index} src={url} alt={`${index + 1}`} loading="lazy" />
+            isVideo(url) ? (
+              <video key={index} controls>
+                <source src={url} type={`video/${getExtensionFromUrl(url)}`} />
+                Your browser does not support the video tag.
+              </video>
+            ) : (
+              <>Unknown Data</>
+            )
+          )}
+          {filteredImages.length >= 4 && (
+            <div className="more-images-overlay">
+              {filteredImages.length > 5 && <span>+{images.length - 4}</span>}
+              <FeedCardImageItem url={images[3]} index={3} />
+              {filteredImages.slice(4).map((url, index) => (
+                <FeedCardImageItem url={url} index={index + 4} hidden={true} />
+              ))}
+
+              {/* <img key={1} src={images[3]} alt={`4`} loading="lazy" /> */}
+            </div>
+          )}
+        </div>
+      </Gallery>
     </div>
   );
 };
---
+++ src/main/front/src/components/FeedCardImageItem.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { Item } from "react-photoswipe-gallery";
+
+function FeedCardImageItem({ url, index, hidden }) {
+  const [dimensions, setDimensions] = useState({ width: 300, height: 300 });
+
+  useEffect(() => {
+    const loadDimensions = async () => {
+      try {
+        const { width, height } = await preloadImage(url);
+        setDimensions({ width, height });
+      } catch (error) {
+        console.error("Failed to load image dimensions", error);
+      }
+    };
+
+    loadDimensions();
+  }, [url]);
+
+  return (
+    <Item
+      original={url}
+      thumbnail={url}
+      width={dimensions.width}
+      height={dimensions.height}
+    >
+      {({ ref, open }) => (
+        <img
+          ref={ref}
+          onClick={open}
+          src={url}
+          alt={`image-${index}`}
+          className="thumbnail"
+          loading="lazy"
+          style={
+            { display: hidden ? "none" : "block" }
+            // { display: "block" }
+          }
+        />
+      )}
+    </Item>
+  );
+}
+
+export default FeedCardImageItem;
+
+const preloadImage = (src) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => resolve({ width: img.width, height: img.height });
+    img.onerror = reject;
+  });
+};
