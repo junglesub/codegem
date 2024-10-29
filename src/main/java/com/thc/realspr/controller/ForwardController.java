4957eacd27fafb70eb295a674b84dfd9f5326d5e
Jungsub Ryoo <junglesubmarine@gmail.com>
2024-10-29 15:54:08 +0900

    fix(server): admin route forward (#35)

--- src/main/java/com/thc/realspr/controller/ForwardController.java
+++ src/main/java/com/thc/realspr/controller/ForwardController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ForwardController {
-    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}"  })
+    @RequestMapping(value = {"/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}", "/admin/*"})
     public String index() {
         return "forward:/index.html";
     }
