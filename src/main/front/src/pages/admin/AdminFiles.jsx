4fa73f9459288d57afc1158299f3f6162cbeae01
junglesub <junglesubmarine@gmail.com>
2024-11-02 02:37:48 +0900

    chore(client): handle lint errors

--- src/main/front/src/pages/admin/AdminFiles.jsx
+++ src/main/front/src/pages/admin/AdminFiles.jsx
@@ -1,8 +1,4 @@
-import InfiniteScroll from "react-infinite-scroller";
 import AdminPage from "./AdminPage";
-import FeedCard from "../../components/FeedCard";
-import useLoadData from "../../hooks/useLoadData";
-import { Box, Paper } from "@mui/material";
 import AdminFilesComp from "../../admin/Files";
 
 function AdminFiles() {
