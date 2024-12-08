5ed083ac16eecfa1d51f5aaf4db36c3a47fbe270
Jungsub Ryoo <junglesubmarine@gmail.com>
2024-12-08 18:03:03 +0900

    feat: emergency alert message (#56)

--- src/main/front/src/components/MainDisplay.jsx
+++ src/main/front/src/components/MainDisplay.jsx
@@ -1,4 +1,4 @@
-import { Box, CssBaseline } from "@mui/material";
+import { Box, CssBaseline, Alert, AlertTitle } from "@mui/material";
 import MainDrawer from "../components/MainDrawer";
 
 function MainDisplay({ children, noCount = false }) {
@@ -19,6 +19,22 @@ function MainDisplay({ children, noCount = false }) {
         component="main"
         sx={{ flexGrow: 1, px: 1.5, pb: 6, maxWidth: 700, mx: "auto" }}
       >
+        <Alert variant="filled" severity="error" sx={{ borderRadius: 0 }}>
+          <AlertTitle>
+            새로운 피드 추가가 되지 않는 오류 발생{" "}
+            <a
+              style={{ color: "white" }}
+              href="https://board.handong.app/feed/num/31"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              (#31)
+            </a>
+          </AlertTitle>
+          현재 개발팀이 문제를 해결 중이나, 시험 기간으로 인해 시간이 다소 걸릴
+          수 있습니다.
+        </Alert>
+
         {children}
       </Box>
       {/* <Button variant="contained">Hello world</Button> */}
