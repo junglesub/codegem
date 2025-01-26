30705f01dc7ea085271d19908a0264c2b2c08ae8
junglesub <junglesubmarine@gmail.com>
2025-01-26 19:00:22 +0900

    youtube

--- src/main/front/src/pages/MainScreen.jsx
+++ src/main/front/src/pages/MainScreen.jsx
@@ -21,6 +21,29 @@ const MainScreen = ({ noRedirect = false }) => {
             <GithubLogin />
           </CardContent>
         </Card>
+        <Card sx={{ my: 2 }}>
+          <CardContent>
+            <Typography
+              variant="h5"
+              component="div"
+              align="center"
+              gutterBottom
+            >
+              시연영상
+            </Typography>
+            <iframe
+              style={{ margin: "auto", display: "block" }}
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/17Xo9NdE4wM?si=lFr2HN5aaMq5l7cc"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </CardContent>
+        </Card>
       </MainDisplay>
     </>
   );
